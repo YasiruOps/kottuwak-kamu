712d89812964118554c1f55c607d477e31634cb7
YasiruOps <it20011802@my.sliit.lk>
2022-08-18 00:34:27 +0530

    ver3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import React from 'react'
 import Video1 from "./zombie.mp4"
@@ -7,8 +8,8 @@ import Vid2 from "./tessay.mp4"
 
 function App() {
   return (
-      <div className='outermain'>
-          <video className='video'  src={Video1} autoPlay loop muted/>
+      <div className='outermain row'>
+          <video className='video col-12'  src={Video1} autoPlay loop muted/>
           <p className="ammo"> කොත්තුවක් කමුතෙ</p>
           </div>
   );
